98a89e704c29723d60ca1cb2836c71ad8765f1d9
Diegoarango20 <diego_arango7@hotmail.com>
2025-03-21 21:21:39 -0500

    solucion3

--- actividad3/src/main/java/com/example/CodificandoMensajeSecreto.java
+++ actividad3/src/main/java/com/example/CodificandoMensajeSecreto.java
@@ -2,9 +2,30 @@ package com.example;
 
 public class CodificandoMensajeSecreto {
     public static void main(String[] args) {
-        
-        System.out.println("Hola Profe no pude con este");
 
+        // Convierte los números a letras
+
+        String mensajeCodificado = "H0l4  MUnD0,  3st0  3s  Un  m3ns4j3  s3cr3t0";
+
+        String mensajeDecodificado = mensajeCodificado
+
+                .replace('0', 'o')
+                .replace('3', 'e')
+                .replace('4', 'a');
+
+        // Elimina los espacios en blanco adicionale
+
+        mensajeDecodificado = mensajeDecodificado.trim();
+        mensajeDecodificado = mensajeDecodificado.replaceAll("\\s+", " "); 
+
+
+        // Convierte la cadena a mayúsculas
+
+        mensajeDecodificado = mensajeDecodificado.toUpperCase();
+
+        // Resultado
+
+        System.out.println("El mnsaje decodificado es: " + mensajeDecodificado);
 
     }
 }
